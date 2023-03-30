9a88dd2f09468ebf7dd1d2ad7755993923f70e7f
data <data.shkubuliani@redberry.com>
2023-03-30 22:37:36 +0400

    feat: add new method

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quote;
 use Illuminate\Http\Request;
 
 class QuoteController extends Controller
 {
-    //
+    public function index(Quote $quote)
+    {
+        return view('home', [
+            'quote' => $quote->inRandomOrder()->first()
+        ]);
+    }
 }
