a3a2240322992c6bb6f44fd1d0c09d1a6204f892
data <data.shkubuliani@redberry.com>
2023-03-30 22:36:12 +0400

    style: update markup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,9 @@
 <x-layout>
-    <h1 class="text-rose-600">Test</h1>
+    <div class="min-h-screen flex items-center justify-center flex-col">
+        {{-- Test image --}}
+        <img src="https://server5.intermedia.ge/article_images/small/201905/2019051700011137332.jpg"
+             alt="movie picture"
+        />
+        <h1>{{ $quote->name }}</h1>
+    </div>
 </x-layout>
