df809d88c953cbcb9eb5b0c9367288b0178b48e5
data <data.shkubuliani@redberry.com>
2023-03-30 21:19:52 +0400

    feat: add new columns in quotes table

--- database/migrations/2023_03_30_170140_create_quotes_table.php
+++ database/migrations/2023_03_30_170140_create_quotes_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('quotes', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('movie_id');
+            $table->string('movie_picture');
             $table->timestamps();
         });
     }
