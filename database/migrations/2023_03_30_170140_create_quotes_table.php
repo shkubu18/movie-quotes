ff5a2167f5eb328038b459ac6459d0e0a4d3ddc1
data <data.shkubuliani@redberry.com>
2023-03-31 13:44:57 +0400

    fix: update movie_id column

--- database/migrations/2023_03_30_170140_create_quotes_table.php
+++ database/migrations/2023_03_30_170140_create_quotes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('quotes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('movie_id');
+            $table->foreignId('movie_id')->constrained()->cascadeOnDelete();
             $table->string('movie_picture')->nullable();
             $table->timestamps();
         });
