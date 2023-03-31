3b18082b4bf0dd5acc4aa60bc6440450838ff611
data <data.shkubuliani@redberry.com>
2023-03-31 18:14:14 +0400

    feat: add new method which render movie view file and shows movie with its quotes

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function show(Movie $movie)
+    {
+        return view('movie', [
+            'movie' => $movie,
+            'quotes' => $movie->quotes
+        ]);
+    }
 }
