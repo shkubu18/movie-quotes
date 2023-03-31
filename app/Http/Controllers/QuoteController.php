154c0cf5839505716b5f8c778983a10a55bcca01
data <data.shkubuliani@redberry.com>
2023-04-01 01:51:09 +0400

    refactor: add return type to method

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Quote;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class QuoteController extends Controller
 {
-    public function index(Quote $quote)
+    public function index(Quote $quote): View
     {
         return view('home', [
             'quote' => $quote->inRandomOrder()->first()
