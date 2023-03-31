0ee125c9f35061be308a3bd6a63b0440afe9da37
data <data.shkubuliani@redberry.com>
2023-03-31 19:03:20 +0400

    refactor: add return type to method

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Movie extends Model
 {
     use HasFactory;
 
-    public function quotes()
+    public function quotes(): HasMany
     {
         return $this->hasMany(Quote::class);
     }
