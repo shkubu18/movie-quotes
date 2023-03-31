764f673227ee1fe32a248a6f00a67c651ec75005
data <data.shkubuliani@redberry.com>
2023-03-31 20:44:06 +0400

    style: update markup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
     @if($quote)
         <div class="min-h-screen flex items-center justify-center flex-col">
+            <a class="mb-5 underline text-xl" href="/login">Login</a>
             {{-- Test image --}}
             <img src="https://server5.intermedia.ge/article_images/small/201905/2019051700011137332.jpg"
                  alt="movie picture"
@@ -14,6 +15,7 @@
             </a>
         </div>
         @else
+        <a class="text-center" href="/login">Login</a>
         <p class="text-center">No quotes yet. Please check back later.</p>
     @endif
 </x-layout>
