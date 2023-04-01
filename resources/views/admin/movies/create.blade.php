086c60f65fa2e9b109e99b0a22638af50ba668ff
data <data.shkubuliani@redberry.com>
2023-04-01 22:16:07 +0400

    style: update markup

--- resources/views/admin/movies/create.blade.php
+++ resources/views/admin/movies/create.blade.php
@@ -1,3 +1,35 @@
 <x-layout>
-    <h1>movies form</h1>
+    <div class="min-h-screen flex items-center justify-center flex-col">
+        <h1 class="mb-10 text-3xl">Add new movie</h1>
+        <form class="w-1/5" action="/admin/movies" method="POST">
+            @csrf
+
+            <div class="flex flex-col">
+                <label class="text-lg" for="name">Movie Name</label>
+                <input
+                    class="border border-gray-200 p-2 rounded"
+                    type="text"
+                    name="name"
+                    id="name"
+                    value="{{ old('name') }}"
+                    required
+                />
+            </div>
+            <div class="flex flex-col mt-3">
+                <label class="text-lg" for="slug">Slug</label>
+                <input
+                    class="border border-gray-200 p-2 rounded"
+                    type="text"
+                    name="slug"
+                    id="slug"
+                    value="{{ old('slug') }}"
+                    required
+                />
+            </div>
+            @error('warning')
+            <p class="text-red-500 text-xs font-semibold">{{ $message }}</p>
+            @enderror
+            <button class="bg-sky-500 p-2 rounded text-white mt-8 w-full" type="submit">Create new movie</button>
+        </form>
+    </div>
 </x-layout>
