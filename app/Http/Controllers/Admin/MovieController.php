fdbc9ef57dd3beaac100528166e6ac3fb05ce42c
data <data.shkubuliani@redberry.com>
2023-04-01 22:12:32 +0400

    feat: add new methods for view form to add new movies and store validated movies in database

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class MovieController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('admin.movies.create');
+    }
+
+    public function store()
+    {
+        Movie::create($this->validateMovie());
+
+        return redirect()->route('home');
+    }
+
+    protected function validateMovie(?Movie $movie = null): array
+    {
+        $movie ??= new Movie();
+
+        return request()->validate([
+            'name' => 'required',
+            'slug' => ['required', Rule::unique('movies', 'slug')->ignore($movie)],
+        ]);
+    }
 }
