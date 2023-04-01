739ce202abcbbbfbcd39e89f0fe4b486bbde24f7
data <data.shkubuliani@redberry.com>
2023-04-02 00:04:04 +0400

    chore: add guarded property

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -10,6 +10,8 @@ class Quote extends Model
 {
     use HasFactory;
 
+    protected $guarded = ['id'];
+
     public function movie(): BelongsTo
     {
         return $this->belongsTo(Movie::class);
