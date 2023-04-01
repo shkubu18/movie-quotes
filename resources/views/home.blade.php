4d5a9bf073c86fb5693bffd9e335fde0ea882a76
data <data.shkubuliani@redberry.com>
2023-04-02 00:07:05 +0400

    style: update markup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
             <h1 class="font-bold text-center">Welcome {{ auth()->user()->name }}!</h1>
 
             <a class="underline mr-5" href="/admin/movies/create">Add new movie</a>
+            <a class="underline mr-5" href="/admin/quotes/create">Add new quote</a>
 
             <button form="logout-form" class="underline" type="submit">Log Out</button>
             <form id="logout-form" class="hidden" action="/logout" method="POST">
@@ -18,9 +19,10 @@
 
     @if($quote)
         <div class="min-h-screen flex items-center justify-center flex-col">
-            {{-- Test image --}}
-            <img src="https://server5.intermedia.ge/article_images/small/201905/2019051700011137332.jpg"
-                 alt="movie picture"
+            <img
+                class="w-2/5 rounded"
+                src="{{ asset('storage/' . $quote->movie_picture) }}"
+                alt="movie picture"
             />
             <h1>{{ $quote->name }}</h1>
             <a
--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -2,9 +2,10 @@
     <div class="min-h-screen flex items-center justify-center flex-col">
         <h1>{{ $movie->name }}</h1>
         @foreach($quotes as $quote)
-            {{-- Test image --}}
-            <img src="https://server5.intermedia.ge/article_images/small/201905/2019051700011137332.jpg"
-                 alt="movie picture"
+            <img
+                class="w-2/5 rounded"
+                src="{{ asset('storage/' . $quote->movie_picture) }}"
+                alt="movie picture"
             />
             <h1>{{ $quote->name }}</h1>
         @endforeach
