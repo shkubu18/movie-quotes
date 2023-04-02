9af12b0e23c5ac86b94621ee5d2b06244332b76c
data <data.shkubuliani@redberry.com>
2023-04-02 23:13:26 +0400

    chore: remove authorize method

--- app/Http/Requests/Movie/StoreMovieRequest.php
+++ app/Http/Requests/Movie/StoreMovieRequest.php
@@ -8,19 +8,6 @@ use Illuminate\Validation\Rule;
 
 class StoreMovieRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
-     */
     public function rules(Movie $movie): array
     {
         return [
