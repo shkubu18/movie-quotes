5f32211465362d56455854789b0d467362e62109
data <data.shkubuliani@redberry.com>
2023-04-02 23:19:22 +0400

    feat: add rules for new quotes

--- app/Http/Requests/Quote/StoreQuoteRequest.php
+++ app/Http/Requests/Quote/StoreQuoteRequest.php
@@ -3,13 +3,16 @@
 namespace App\Http\Requests\Quote;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreQuoteRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'movie_picture' => 'required|image',
+            'movie_id' => ['required', Rule::exists('movies', 'id')]
         ];
     }
 }
