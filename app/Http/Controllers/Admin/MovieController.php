3e445772138cbca7f2f81f25369341bc7ea2a1ca
data <data.shkubuliani@redberry.ge>
2023-04-04 02:36:19 +0400

    feat: add new methods for view dashboard for movies and also view form where user update,delete movies

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -4,26 +4,56 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Movie\StoreMovieRequest;
+use App\Http\Requests\Movie\UpdateMovieRequest;
 use App\Models\Movie;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
 
 class MovieController extends Controller
 {
-    public function create(): View
-    {
-        return view('admin.movies.create');
-    }
+	public function index(): View
+	{
+		return view('admin.movies.dashboard', [
+			'movies' => Movie::all(),
+		]);
+	}
 
-    public function store(StoreMovieRequest $request): RedirectResponse
-    {
-        Movie::create([
-            'name' => $request->name,
-            'slug' => $request->slug
-        ]);
+	public function create(): View
+	{
+		return view('admin.movies.create');
+	}
 
-        return redirect()->route('home');
-    }
+	public function store(StoreMovieRequest $request): RedirectResponse
+	{
+		Movie::create([
+			'name' => $request->name,
+			'slug' => $request->slug,
+		]);
+
+		return redirect()->route('home');
+	}
+
+	public function edit(Movie $movie): View
+	{
+		return view('admin.movies.edit', [
+			'movie' => $movie,
+		]);
+	}
+
+	public function update(UpdateMovieRequest $request, Movie $movie): RedirectResponse
+	{
+		$movie->update([
+			'name' => $request->name,
+			'slug' => $request->slug,
+		]);
+
+		return redirect()->route('movies_dashboard');
+	}
+
+	public function destroy(Movie $movie): RedirectResponse
+	{
+		$movie->delete();
+
+		return back();
+	}
 }
