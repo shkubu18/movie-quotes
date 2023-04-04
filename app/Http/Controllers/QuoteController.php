e5e60fc9faf7a109be135313e88192084861b0da
data <data.shkubuliani@redberry.ge>
2023-04-04 18:14:57 +0400

    feat: add new method for for view specific quote

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Quote;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class QuoteController extends Controller
 {
-    public function index(Quote $quote): View
-    {
-        return view('home', [
-            'quote' => $quote->inRandomOrder()->first()
-        ]);
-    }
+	public function index(Quote $quote): View
+	{
+		return view('home', [
+			'quote' => $quote->inRandomOrder()->first(),
+		]);
+	}
+
+	public function show(Quote $quote): View
+	{
+		return view('admin.quotes.show', [
+			'quote'  => $quote,
+		]);
+	}
 }
