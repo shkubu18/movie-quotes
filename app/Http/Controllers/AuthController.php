eab3274f8062465f162344b7f584a05ef2b38faf
data <data.shkubuliani@redberry.ge>
2023-04-04 21:36:55 +0400

    chore: remove create method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,39 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AuthAttemptRequest;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
-use Illuminate\View\View;
 
 class AuthController extends Controller
 {
-    public function create(): View
-    {
-        return view('login');
-    }
+	public function store(AuthAttemptRequest $request): RedirectResponse
+	{
+		$attributes = [
+			'email'    => $request->email,
+			'password' => $request->password,
+		];
 
-    public function store(AuthAttemptRequest $request): RedirectResponse
-    {
-        $attributes = [
-            'email' => $request->email,
-            'password' => $request->password
-        ];
+		if (!auth()->attempt($attributes))
+		{
+			throw ValidationException::withMessages([
+				'warning' => 'Incorrect email or password.',
+			]);
+		}
 
-        if (!auth()->attempt($attributes)) {
-            throw ValidationException::withMessages([
-                'warning' => 'Incorrect email or password.'
-            ]);
-        }
+		session()->regenerate();
 
-        session()->regenerate();
+		return redirect()->route('home');
+	}
 
-        return redirect()->route('home');
-    }
+	public function destroy(): RedirectResponse
+	{
+		auth()->logout();
 
-    public function destroy(): RedirectResponse
-    {
-        auth()->logout();
-
-        return redirect()->route('home');
-    }
+		return redirect()->route('home');
+	}
 }
