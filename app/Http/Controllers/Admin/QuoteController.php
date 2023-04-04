b7d6be2a4c5671b0a9f19b1cbb93150c241199b3
data <data.shkubuliani@redberry.ge>
2023-04-04 17:34:03 +0400

    feat: add new methods for crud operations

--- app/Http/Controllers/Admin/QuoteController.php
+++ app/Http/Controllers/Admin/QuoteController.php
@@ -4,34 +4,69 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Quote\StoreQuoteRequest;
+use App\Http\Requests\Quote\UpdateQuoteRequest;
 use App\Models\Movie;
 use App\Models\Quote;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
 
 class QuoteController extends Controller
 {
-    public function create(): View
-    {
-        return view('admin.quotes.create', [
-            'movies' => Movie::all()
-        ]);
-    }
+	public function index(): View
+	{
+		return view('admin.quotes.dashboard', [
+			'quotes' => Quote::all(),
+		]);
+	}
 
-    public function store(StoreQuoteRequest $request): RedirectResponse
-    {
-        $attributes = [
-            'name' => $request->name,
-            'movie_picture' => $request->movie_picture,
-            'movie_id' => $request->movie_id
-        ];
+	public function create(): View
+	{
+		return view('admin.quotes.create', [
+			'movies' => Movie::all(),
+		]);
+	}
 
-        $attributes['movie_picture'] = request()->file('movie_picture')->store('pictures');
+	public function store(StoreQuoteRequest $request): RedirectResponse
+	{
+		$attributes = [
+			'name'          => $request->name,
+			'movie_picture' => $request->movie_picture,
+			'movie_id'      => $request->movie_id,
+		];
 
-        Quote::create($attributes);
+		$attributes['movie_picture'] = request()->file('movie_picture')->store('pictures');
 
-        return redirect()->route('home');
-    }
+		Quote::create($attributes);
+
+		return redirect()->route('home');
+	}
+
+	public function edit(Quote $quote): View
+	{
+		return view('admin.quotes.edit', [
+			'quote'  => $quote,
+			'movies' => Movie::all(),
+		]);
+	}
+
+	public function update(UpdateQuoteRequest $request, Quote $quote): RedirectResponse
+	{
+		$attributes = $request->validated();
+
+		if ($attributes['movie_picture'] ?? false)
+		{
+			$attributes['movie_picture'] = request()->file('movie_picture')->store('pictures');
+		}
+
+		$quote->update($attributes);
+
+		return redirect()->route('quotes_dashboard');
+	}
+
+	public function destroy(Quote $quote): RedirectResponse
+	{
+		$quote->delete();
+
+		return back();
+	}
 }
