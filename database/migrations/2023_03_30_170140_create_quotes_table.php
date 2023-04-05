097ab3262c73baf0e06e5e61d53fa748b79baf38
data <data.shkubuliani@redberry.ge>
2023-04-05 13:51:23 +0400

    chore: remove slug column

--- database/migrations/2023_03_30_170140_create_quotes_table.php
+++ database/migrations/2023_03_30_170140_create_quotes_table.php
@@ -13,7 +13,6 @@ return new class() extends Migration {
 		Schema::create('quotes', function (Blueprint $table) {
 			$table->id();
 			$table->string('name');
-			$table->string('slug')->unique();
 			$table->foreignId('movie_id')->constrained()->cascadeOnDelete();
 			$table->string('movie_picture')->nullable();
 			$table->timestamps();
