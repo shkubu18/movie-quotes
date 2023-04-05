5b660301e64b0b1469d0356a940998dfb7bbfdba
data <data.shkubuliani@redberry.ge>
2023-04-05 13:52:31 +0400

    style: update markup

--- resources/views/admin/quotes/dashboard.blade.php
+++ resources/views/admin/quotes/dashboard.blade.php
@@ -2,9 +2,15 @@
     <div class="flex justify-center items-center min-h-screen">
         <div class="flex flex-col items-center rounded p-10 w-2/5">
             @foreach($quotes as $quote)
-                <div class="flex justify-between bg-slate-200 rounded p-4 w-full {{ !$loop->first ? ' mt-3' : '' }}">
-                    <a class="mr-5" href="/quotes/{{ $quote->slug}}">{{ $quote->name }}</a>
-                    <div class="flex">
+                <div class="flex items-center justify-between bg-slate-200 rounded p-4 w-full {{ !$loop->first ? ' mt-3' : '' }}">
+                    <h2 class="mr-5">{{ $quote->name }}</h2>
+                    <div class="flex items-center">
+                        <img
+                            width="100"
+                            class="mr-5 rounded"
+                            src="{{ asset('storage/' . $quote->movie_picture) }}"
+                            alt="movie picture"
+                        >
                         <a class="mr-5" href="/admin/quotes/{{ $quote->id }}/edit">Edit</a>
                         <form action="/admin/quotes/{{ $quote->id }}" method="POST">
                             @csrf
