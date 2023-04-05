dd881f977636fc879359c5ac047568b6e8cfb188
data <data.shkubuliani@redberry.ge>
2023-04-05 20:13:31 +0400

    chore: update movies table

--- database/migrations/2023_03_30_125221_create_movies_table.php
+++ database/migrations/2023_03_30_125221_create_movies_table.php
@@ -4,25 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('movies', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('slug')->unique();
-            $table->timestamps();
-        });
-    }
+return new class() extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('movies', function (Blueprint $table) {
+			$table->id();
+			$table->json('name');
+			$table->string('slug')->unique();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('movies');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('movies');
+	}
 };
