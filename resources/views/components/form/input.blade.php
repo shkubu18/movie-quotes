eb4764b9f2ebec603112c6314c85e5b4913e6e2c
data <data.shkubuliani@redberry.ge>
2023-04-08 22:46:16 +0400

    style: update input component padding and label component text size

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -3,8 +3,7 @@
 <x-form.field>
     <x-form.label name="{{ $name }}" title="{{ $label }}" />
     <input
-        {{ $attributes(['class' => 'border border-gray-200 p-2 rounded text-black']) }}
-        class="border border-gray-200 p-2 rounded text-black"
+        {{ $attributes(['class' => 'border border-gray-200 p-2.5 rounded text-black']) }}
         name="{{ $name }}"
         id="{{ $name }}"
         value="{{ old($name) }}"
--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -1,5 +1,5 @@
 @props(['name', 'title'])
 
-<label class="text-lg mb-1" for="{{ $name }}">
+<label class="text-xl mb-1" for="{{ $name }}">
     {{ __($title) }}
 </label>
