1e7945f9863ae29a83ca0c58545325d59e0eefc9
data <data.shkubuliani@redberry.ge>
2023-04-08 22:44:44 +0400

    chore: add texts for inpute placeholders

--- lang/en/movie_form.php
+++ lang/en/movie_form.php
@@ -2,9 +2,12 @@
 
 return [
 	'heading'                 => 'Add new movie',
-	'movie_name_en'           => 'Movie Name (English)',
-	'movie_name_ka'           => 'Movie Name (Georgian)',
+	'name_en'                 => 'Movie Name (English)',
+	'name_ka'                 => 'Movie Name (Georgian)',
+	'name_en_placeholder'     => 'Movie name in English',
+	'name_ka_placeholder'     => 'Movie name in Georgian',
 	'slug'                    => 'Slug',
+	'slug_placeholder'        => 'slug',
 	'button'                  => 'Create new movie',
 	'edit_heading'            => 'Edit movie',
 	'update_btn'              => 'Update',
