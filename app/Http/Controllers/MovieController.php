d40aa0e3d808b0501e0087eb3a65ae74a51c45f0
data <data.shkubuliani@redberry.ge>
2023-04-12 19:33:30 +0400

    refactor: delete Admin folder and its controllers and move their methods in movie and quotes controllers

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,11 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Movie\StoreMovieRequest;
+use App\Http\Requests\Movie\UpdateMovieRequest;
 use App\Models\Movie;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 
 class MovieController extends Controller
 {
+	public function index(): View
+	{
+		return view('admin.movies.index', [
+			'movies' => Movie::all(),
+		]);
+	}
+
+	public function store(StoreMovieRequest $request): RedirectResponse
+	{
+		Movie::create($request->validated());
+
+		return redirect()->route('quotes.show');
+	}
+
+	public function edit(Movie $movie): View
+	{
+		return view('admin.movies.edit', [
+			'movie' => $movie,
+		]);
+	}
+
+	public function update(UpdateMovieRequest $request, Movie $movie): RedirectResponse
+	{
+		$movie->update($request->validated());
+
+		return redirect()->route('movies.index');
+	}
+
+	public function destroy(Movie $movie): RedirectResponse
+	{
+		$movie->delete();
+
+		return back();
+	}
+
 	public function show(Movie $movie): View
 	{
 		return view('movies.show', [
